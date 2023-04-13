e2d0acc1084aae3ebd1b2767c0eb42b4a67fa800
marcuslee8888 <101726033+marcuslee8888@users.noreply.github.com>
2023-04-13 15:10:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.crg14tswidqf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "marcuslee-employee"
+customregion = "us-east-1"
